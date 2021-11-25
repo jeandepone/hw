60ac9fab3096a07a7c6f8c8d70fc256054d3da66
Julien Fraichard <julienfr112@gmail.com>
2021-11-25 14:33:56 +0100

    move to postgres aiohttp

--- server.py
+++ server.py
@@ -1,24 +1,39 @@
-import tornado.ioloop
-import tornado.web
-import motor.motor_tornado
+from aiohttp import web
+from aiohttp.web_response import json_response, Response
+import asyncpg
 import os
-
-client = motor.motor_tornado.MotorClient(
-    os.environ.get("MONGO_URI", "mongodb://localhost:27017")
-)
+import json
 
 
-class MainHandler(tornado.web.RequestHandler):
-    def post(self):
-        self.write(self.request.body)
-
-    def get(self):
-        self.write("Hello, world")
+async def get(request):
+    async with request.app["pool"].acquire() as conn:
+        values = list(await conn.fetch("SELECT data::json FROM table1"))
+    return json_response({"values": [v for v, in values]})
 
 
-tornado.web.Application(
-    [
-        (r"/", MainHandler),
-    ]
-).listen(int(os.environ.get("PORT", 8888)))
-tornado.ioloop.IOLoop.current().start()
+async def post(request):
+    async with request.app["pool"].acquire() as conn:
+        await conn.execute(
+            "insert into table1(data) values ($1::json)", {"cou": "robert"}
+        )
+    return Response()
+
+
+async def app():
+    app = web.Application()
+    pool = await asyncpg.create_pool(
+        os.environ.get("DATABASE_URL", "postgres://test:testpwd@localhost:5432/test")
+    )
+    async with pool.acquire() as conn:
+        #await conn.execute("drop table if exists table1")
+        await conn.execute("create table if not exists table1(id Serial, data jsonb)")
+        await conn.set_type_codec(
+            "json", encoder=json.dumps, decoder=json.loads, schema="pg_catalog"
+        )
+    app["pool"] = pool
+    app.add_routes([web.get("/coucou", get), web.post("/", post)])
+    return app
+
+
+if __name__ == "__main__":
+    web.run_app(app())
